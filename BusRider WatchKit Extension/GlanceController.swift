0560b54d029490a4ca294233dceaf007da4742d6
John Wong <huangxiaozhe1988@gmail.com>
2015-04-07 01:15:37 +0800

    Start watch kit in swift

---
+++ BusRider WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  BusRider WatchKit Extension
+//
+//  Created by John Wong on 4/7/15.
+//  Copyright (c) 2015 John Wong. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
